3f846025f7fac3a1c2efefeb61814e86940bf3bc
Henrique-Kriguer <henriquekriguer@gmail.com>
2023-02-22 17:07:24 -0300

    Better way to handle the exceptions

---
+++ src/Program/application.java
@@ -0,0 +1,26 @@
+package Program;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class application {
+
+    public static void main(String[] args) {
+
+        String path = "C:\\Development\\Nelio_JAVA\\17-Files\\HenriqueKriguer.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+            String line = br.readLine();
+
+            while( line != null){
+                System.out.println(line);
+                line = br.readLine();
+            }
+        }
+        catch(IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+}
